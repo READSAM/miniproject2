8c05175a8a0a07b08a262b9f14cdbbf284f71daf
Samriddhi <157903700+READSAM@users.noreply.github.com>
2024-10-08 20:25:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let userScore=0;
+let compScore=0;
+let msg=document.querySelector("#msg");
+let User=document.querySelector("#User");
+let CPU=document.querySelector("#CPU");
+let reset=document.querySelector("#reset");
+const options=document.querySelectorAll(".options");
+
+reset.onclick=()=>
+{
+    userScore=0;
+    compScore=0;
+    User.innerText=userScore;
+    CPU.innerText=compScore;
+}
+const draw=()=>
+{
+    msg.innerText="Draw :|";
+}
+const showWinner=(win)=>
+{
+    User.innerText=userScore;
+    CPU.innerText=compScore;
+    msg.innerText=win===true?" You win :)":" You lose.";
+
+}
+const play=(uc)=>{ 
+    const cpu=cpuChoice();
+    if(cpu==uc) draw();
+    else 
+    {
+        let win=true;
+        if(uc==="rock") win=cpu==="paper"?false:true;
+    
+        else if (uc==="paper") win=cpu==="scissors"?false:true;
+        else win=cpu==="rock"?false:true;
+        showWinner(win);
+        if(win===true) userScore++;
+        else compScore++;
+    }
+}
+options.forEach((op)=>{
+      op.addEventListener("click",()=>{ 
+        let userChoice=op.getAttribute("id");
+        play(userChoice);
+      });
+});
+
+const cpuChoice=()=>
+{
+    const choiceList=["rock","paper","scissors"];
+     let rl= Math.floor(Math.random()*3);
+     return choiceList[rl];
+}
+
